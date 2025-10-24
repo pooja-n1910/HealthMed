c516773738afbdcc6f808dec683a3a687b788424
pooj-n1910 <poojanaganna19@gmail.com>
2025-10-24 15:00:16 +0530

    Fix port binding for Render deployment

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import send_file
+from flask import Flask, render_template, redirect, url_for, request, session, send_file
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
 from weasyprint import HTML
 from datetime import datetime
 import io
+import os
 
-from flask import Flask, render_template, redirect, url_for, request, session
-from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin
 from forms import LoginForm
 import config
 
@@ -95,9 +95,9 @@ def invoice():
     pdf = HTML(string=html).write_pdf()
     return send_file(io.BytesIO(pdf), download_name="invoice.pdf", as_attachment=True)
 
-
 # Run the app
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
